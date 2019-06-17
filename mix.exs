9d4faa4e18c7337172ac6641e77df3e7417e661b
Christian Blavier <cblavier@gmail.com>
2019-06-17 16:45:30 +0200

    added needed packages (omxplayer, curl, python)

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
-defmodule NervesSystemRpi3.MixProject do
+defmodule PhenixRpi3.MixProject do
   use Mix.Project
 
-  @app :nerves_system_rpi3
+  @app :phenix_rpi3
   @version Path.join(__DIR__, "VERSION")
            |> File.read!()
            |> String.trim()
@@ -34,9 +34,9 @@ defmodule NervesSystemRpi3.MixProject do
   defp nerves_package do
     [
       type: :system,
-      artifact_sites: [
-        {:github_releases, "nerves-project/#{@app}"}
-      ],
+      # artifact_sites: [
+      #   {:github_releases, "nerves-project/#{@app}"}
+      # ],
       build_runner_opts: build_runner_opts(),
       platform: Nerves.System.BR,
       platform_config: [
